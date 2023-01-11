e54d09d8f965242312e52c9779cd9dbac0a361f1
Karthick <pkarthicbz@live.in>
2023-01-11 21:55:47 +0530

    make module to handle all play modes

--- scripts/script.js
+++ scripts/script.js
@@ -24,25 +24,19 @@ const gameBoard = (()=>{
         const checkSameValue = (currentValue)=>currentValue == player.value;
         let j=0;
         for(let i=0; i<9; i+=3){
-            // console.log(gameArray.slice(i, i+3));
-            // console.log([gameArray[j], gameArray[j+3], gameArray[j+6]]);
             if(gameArray.slice(i, i+3).every(checkSameValue) == true){
-                // gameArray.length = 0;
                 makeGamepad();
                 gameBoardActivities.changeWinnerColor(gameArray.slice(i, i+3));
                 return true;
             }else if([gameArray[j], gameArray[j+3], gameArray[j+6]].every(checkSameValue) == true){
-                // gameArray.length = 0;
                 makeGamepad();
                 gameBoardActivities.changeWinnerColor([gameArray[j], gameArray[j+3], gameArray[j+6]]);
                 return true;
             }else if([gameArray[0], gameArray[4], gameArray[8]].every(checkSameValue) == true){
-                // gameArray.length = 0;
                 makeGamepad();
                 gameBoardActivities.changeWinnerColor([gameArray[0], gameArray[4], gameArray[8]]);
                 return true;
             }else if([gameArray[2], gameArray[4], gameArray[6]].every(checkSameValue) == true){
-                // gameArray.length = 0;
                 makeGamepad();
                 gameBoardActivities.changeWinnerColor([gameArray[2], gameArray[4], gameArray[6]]);
                 return true
@@ -50,6 +44,64 @@ const gameBoard = (()=>{
                 j+=1;
             }
         }
+    }
+
+    return {addItemToArray, makeGamepad, checkWinner, rookieAi, gameArray};
+})();
+
+const players = (value)=>{
+    return {value};
+}
+
+const player1 = players('X');
+const player2 = players('O');
+
+let counter = 0;
+let lastPlayer;
+
+gameBoard.makeGamepad();
+
+const playModes = (()=>{
+    function userPlay(){
+        cellContainer.forEach(cells =>{
+            cells.addEventListener('click', (e)=>{
+                // gameBoard.makeGamepad();
+                if(!playAgainActive){
+                if(counter <8){
+                    console.log(counter);
+                    if(counter %2 == 0){
+                        e.target.innerText = player1.value;
+                        lastPlayer = player1.value;
+                        gameBoard.addItemToArray(e.target.dataset.cell-1, player1);
+                        if(gameBoard.checkWinner(player1)){
+                            heading.textContent = `${player1.value} Wins!`;
+                            gameBoardActivities.displayPlayAgain();
+                            
+                        }
+                    }else{
+                        e.target.innerText = player2.value;
+                        lastPlayer = player2.value;
+                        gameBoard.addItemToArray(e.target.dataset.cell-1, player2);
+                        if(gameBoard.checkWinner(player2)){
+                            heading.textContent = `${player2.value} Wins`;
+                            gameBoardActivities.displayPlayAgain();
+                        }
+                    }
+                }else{
+                    if(lastPlayer === 'X'){
+                        e.target.innerText = 'O';
+                    }else{
+                        e.target.innerText = 'X';
+                    }
+                    heading.textContent = 'Game Draw!';
+                    gameBoard.makeGamepad();
+                    gameBoardActivities.displayPlayAgain();
+                    counter = 0;
+                }
+            }
+                counter++;
+            });
+        });
     }
 
     const rookieAi = ()=>{
@@ -63,92 +115,49 @@ const gameBoard = (()=>{
         }
     }
 
-    return {addItemToArray, makeGamepad, checkWinner, rookieAi, gameArray};
-})();
-
-const players = (value)=>{
-    return {value};
-}
-
-// function game(){
-//     const player1 = players('X');
-//     const player2 = players('O');
-//     gameBoard.makeGamepad();
-// }
-
-const player1 = players('X');
-const player2 = players('O');
-// gameBoard.makeGamepad();
-// for(let i=0; i<9; i++){
-//     if(i%2 == 0){
-
-//         console.log('Player 1 playing');
-//         gameBoard.addItemToArray(gameBoard.rookieAi(), player1);
-//         // console.log(gameBoard.checkWinner(player1));
-//         if(gameBoard.checkWinner(player1) == true){
-//             console.log(`player${player1.value} wins`)
-//             break;
-//         }
-//     }else{
-//         console.log('Player 2 playing');
-//         gameBoard.addItemToArray(gameBoard.rookieAi(), player2);
-//         if(gameBoard.checkWinner(player2) == true){
-//             console.log(`player${player2.value} wins`)
-//             break;
-//         }
-//     }
-// }
-
-let counter = 0;
-let lastPlayer;
-
-gameBoard.makeGamepad();
-
-function userPlay(){
-cellContainer.forEach(cells =>{
-    cells.addEventListener('click', (e)=>{
-        // gameBoard.makeGamepad();
-        if(!playAgainActive){
-        if(counter <8){
-            console.log(counter);
-            if(counter %2 == 0){
+    function computerPlay(){
+        let count = 0;
+        cellContainer.forEach(cell=>{
+            cell.addEventListener('click', (e)=>{
+                if(!playAgainActive){
+                console.log(count++);
                 e.target.innerText = player1.value;
-                lastPlayer = player1.value;
                 gameBoard.addItemToArray(e.target.dataset.cell-1, player1);
                 if(gameBoard.checkWinner(player1)){
-                    heading.textContent = `${player1.value} Wins!`;
-                    gameBoardActivities.displayPlayAgain();
-                    
-                }
-            }else{
-                e.target.innerText = player2.value;
-                lastPlayer = player2.value;
-                gameBoard.addItemToArray(e.target.dataset.cell-1, player2);
-                if(gameBoard.checkWinner(player2)){
-                    heading.textContent = `${player2.value} Wins`;
+                    count=0;
                     gameBoardActivities.displayPlayAgain();
                     // gameBoardActivities.displayBackButton();
+                    heading.textContent = 'You won human!';
+                }
+                if(count <= 4){
+                    const aiPosition = minimax(gameBoard.gameArray, player2);
+                    if('index' in aiPosition){
+                        cellContainer[aiPosition.index].innerHTML = player2.value;
+                        gameBoard.addItemToArray(aiPosition.index, player2);
+                        if(gameBoard.checkWinner(player2)){
+                            count=0;
+                            gameBoardActivities.displayPlayAgain();
+                            // gameBoardActivities.displayBackButton();
+                            heading.textContent = 'I beat you!';
+                        }
+                    }
+                }else{
+                    gameBoard.makeGamepad();
+                    count = 0;
+                    gameBoardActivities.displayPlayAgain();
+                    // gameBoardActivities.displayBackButton();
+                    heading.textContent = 'Better luck next time!';
                 }
             }
-        }else{
-            if(lastPlayer === 'X'){
-                e.target.innerText = 'O';
-            }else{
-                e.target.innerText = 'X';
-            }
-            heading.textContent = 'Game Draw!';
-            gameBoard.makeGamepad();
-            gameBoardActivities.displayPlayAgain();
-            // gameBoardActivities.displayBackButton();
-            counter = 0;
-        }
+            });
+        });
     }
-        counter++;
-    });
-});
-}
 
-// const testPlayResults = [];
+    return {computerPlay, rookieAi, userPlay};
+})();
+
+
+
 const checkWinner = (currentBoard, player)=>{
     if(currentBoard[0] === player.value && currentBoard[1] === player.value && currentBoard[2] === player.value ||
         currentBoard[3] === player.value && currentBoard[4] === player.value && currentBoard[5] === player.value ||
@@ -175,7 +184,6 @@ const findEmptyArray = (currentBoard)=>{
 }
 
 const minimax = (currentBoard, player)=>{
-    // console.log(`function call: ${fc++}`);
     const emptyPlaces = findEmptyArray(currentBoard);
 
     if(checkWinner(currentBoard, player1)){
@@ -189,8 +197,6 @@ const minimax = (currentBoard, player)=>{
     const move = [];
 
     for(let i=0; i<emptyPlaces.length; i++){
-        // console.log(i);
-        // console.log(Object.values(move));
         const playScore = {};
         playScore.index = emptyPlaces[i];
         currentBoard[emptyPlaces[i]]=player.value;
@@ -209,7 +215,6 @@ const minimax = (currentBoard, player)=>{
     if(player.value === 'O'){
         let maximum = -Infinity;
         for(let i=0; i<move.length; i++){
-            // console.log(move[i].score);
             if(move[i].score > maximum){
                 maximum = move[i].score
                 bestScore = i;
@@ -218,7 +223,6 @@ const minimax = (currentBoard, player)=>{
     }else{
         let maximum = Infinity;
         for(let i=0; i<move.length; i++){
-            // console.log(move[i].score);
             if(move[i].score < maximum){
                 maximum = move[i].score
                 bestScore = i;
@@ -229,43 +233,7 @@ const minimax = (currentBoard, player)=>{
     return move[bestScore];
 }
 
-function computerPlay(){
-    let count = 0;
-    cellContainer.forEach(cell=>{
-        cell.addEventListener('click', (e)=>{
-            if(!playAgainActive){
-            console.log(count++);
-            e.target.innerText = player1.value;
-            gameBoard.addItemToArray(e.target.dataset.cell-1, player1);
-            if(gameBoard.checkWinner(player1)){
-                count=0;
-                gameBoardActivities.displayPlayAgain();
-                // gameBoardActivities.displayBackButton();
-                heading.textContent = 'You won human!';
-            }
-            if(count <= 4){
-                const aiPosition = minimax(gameBoard.gameArray, player2);
-                if('index' in aiPosition){
-                    cellContainer[aiPosition.index].innerHTML = player2.value;
-                    gameBoard.addItemToArray(aiPosition.index, player2);
-                    if(gameBoard.checkWinner(player2)){
-                        count=0;
-                        gameBoardActivities.displayPlayAgain();
-                        // gameBoardActivities.displayBackButton();
-                        heading.textContent = 'I beat you!';
-                    }
-                }
-            }else{
-                gameBoard.makeGamepad();
-                count = 0;
-                gameBoardActivities.displayPlayAgain();
-                // gameBoardActivities.displayBackButton();
-                heading.textContent = 'Better luck next time!';
-            }
-        }
-        });
-    });
-}
+
 
 const gameBoardActivities = (()=>{
     const clearCells = ()=>{
